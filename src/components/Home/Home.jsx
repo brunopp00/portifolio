4076836af44a9e5ec65bb2fdb70fc7c531750a79
brunof <brunof@unimedvaledocai.com.br>
2023-06-05 15:27:33 -0300

    Alterações na home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,35 @@
 import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import fotoBruno from '../../assets/Foto-Bruno-trasparente.png'
+import './styles.css'
 
 export function Home() {
+  const words = ['Front-end', 'Back-end'] // Adicione mais palavras se desejar
+  const [index, setIndex] = useState(0)
+  const [currentWord, setCurrentWord] = useState(words[0])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length)
+    }, 3000)
+
+    return () => {
+      clearInterval(interval)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    const wordElement = document.getElementById('animation-word')
+    wordElement.classList.remove('active')
+
+    setTimeout(() => {
+      setCurrentWord(words[index])
+      wordElement.classList.add('active')
+    }, 500)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [index])
+
   return (
     <Grid
       id="menu"
@@ -27,7 +54,12 @@ export function Home() {
           variant="h4"
           style={{ fontFamily: 'Tilt Warp, cursive', color: '#2797b2' }}
         >
-          Desenvolvedor Front-End
+          Desenvolvedor
+          <div className="animation-text">
+            <span id="animation-word" className="animation-word">
+              {currentWord}
+            </span>
+          </div>
         </Typography>
       </Grid>
       <Grid item xs={12} md={4} lg={4} style={{ textAlign: 'center' }}>
