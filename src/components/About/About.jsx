aa95bb34c1d32a061f627c6d6025fd951bff4bec
brunopp00 <brunofnh08@gmail.com>
2023-09-25 14:37:11 -0300

    troquei o texto

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,23 +12,23 @@ export function About() {
         </div>
         <div className="flex w-full h-full items-center p-10">
           <p className="font-tilt-warp text-zinc-50 text-sm">
-            Meu nome é Bruno Fröhlich e nasci em 4 de fevereiro de 2006. Desde
-            muito cedo, desenvolver e programar se tornaram minhas paixões.
-            Atualmente, sou um programador com conhecimento em várias linguagens
-            e tecnologias, incluindo JavaScript, TypeScript, Android Java,
-            React.js e Node.js. Atualmente, sou estagiário na Unimed Vale do
-            Caí, onde trabalho com React.js para criar interfaces web. Um dos
-            meus projetos notáveis foi o desenvolvimento de um aplicativo
-            Android que se tornou essencial para todos os funcionários da Unimed
-            Vale do Caí, tornando as operações mais eficientes e eficazes. Meu
-            compromisso com a aprendizagem é evidente, uma vez que concluí o
-            curso de React.js da RocketSeat e estou atualmente ampliando meu
-            conhecimento com Node.js e React Native, também na RocketSeat. Tenho
-            experiência em desenvolvimento fullstack e você pode verificar
-            alguns dos meus projetos na seção de projetos. Além disso, trabalho
-            como freelancer, principalmente na área de front-end, contribuindo
-            para projetos diversos. Meu domínio do Tailwind CSS me permite criar
-            interfaces de usuário elegantes e responsivas. Com um futuro
+            Meu nome é Bruno Fröhlich, nasci em 4 de fevereiro de 2006. Desde
+            muito cedo, desenvolver e programar se tornaram minhas paixões. Sou
+            um programador com conhecimento em várias linguagens e tecnologias,
+            incluindo JavaScript, TypeScript, Android Java, React.js e Node.js.
+            Atualmente, sou estagiário na Unimed Vale do Caí, onde utilizo
+            React.js para desenvolver interfaces web. Destaco um dos meus
+            projetos mais significativos, o desenvolvimento de um aplicativo
+            Android que se tornou fundamental para todos os funcionários da
+            Unimed Vale do Caí, otimizando as operações da empresa. Demonstro
+            meu comprometimento com a aprendizagem através da conclusão do curso
+            de React.js da RocketSeat. Além disso, estou expandindo meu
+            conhecimento em Node.js e React Native, também pela RocketSeat.
+            Tenho experiência em desenvolvimento fullstack e você pode conferir
+            alguns dos meus projetos na seção de projetos. Trabalho como
+            freelancer, principalmente na área de front-end, contribuindo para
+            diversos projetos. Minha habilidade com o Tailwind CSS me permite
+            criar interfaces de usuário elegantes e responsivas. Com um futuro
             promissor pela frente e uma paixão contínua pelo desenvolvimento de
             software, estou ansioso para continuar minha jornada na programação
             e contribuir para projetos inovadores e desafiadores.
